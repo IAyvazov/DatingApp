7a4633039c87cd50aa6a5ab3159ee5294aa1468d
IAyvazov <iayvazovl@gmail.com>
2022-12-01 15:39:14 +0200

    Add member-card and member-details page

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -21,6 +21,8 @@ namespace API.DTOs
 
         public string Introduction { get; set; }
 
+        public string LookingFor { get; set; }
+
         public string Interests { get; set; }
 
         public string City { get; set; }
