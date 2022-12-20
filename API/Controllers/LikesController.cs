b072925f078e06a7b94e606910ac572d07cd4906
IAyvazov <iayvazovl@gmail.com>
2022-12-20 14:23:17 +0200

    Add Likes functionality in API

---
+++ API/Controllers/LikesController.cs
@@ -0,0 +1,59 @@
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class LikesController : BaseApiController
+    {
+        private readonly IUserRepository userRepository;
+        private readonly ILikesRepository likesRepository;
+
+        public LikesController(IUserRepository userRepository, ILikesRepository likesRepository)
+        {
+            this.likesRepository = likesRepository;
+            this.userRepository = userRepository;
+        }
+
+        [HttpPost("{username}")]
+        public async Task<ActionResult> AddLike(string username)
+        {
+            var sourceUserId = this.User.GetUserId();
+
+            var likedUser = await this.userRepository.GetUserByUsernameAsync(username);
+
+            var sourceUser = await this.likesRepository.GetUserWithLikes(sourceUserId);
+
+            if(likedUser == null) return NotFound();
+
+            if(sourceUser.UserName == username) return BadRequest("You cannot like yourself");
+
+            var userLike = await this.likesRepository.GetUserLike(sourceUserId, likedUser.Id);
+
+            if( userLike != null) return BadRequest("You already like this user");
+
+            userLike = new UserLike
+            {
+                SourceUserId = sourceUserId,
+                TargetUserId = likedUser.Id
+            };
+            
+            sourceUser.LikedUsers.Add(userLike);
+
+            if(await this.userRepository.SaveAllAsync()) return Ok();
+
+            return BadRequest("Faild to like user");
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LikeDto>>> GetUserLikes(string predicate)
+        {
+            var users = await this.likesRepository.GetUserLikes(predicate, this.User.GetUserId());
+
+            return Ok(users);
+        }
+    }
+}
---
+++ API/Data/LikesRepository.cs
@@ -0,0 +1,58 @@
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class LikesRepository : ILikesRepository
+    {
+        private readonly DataContext context;
+
+        public LikesRepository(DataContext context)
+        {
+            this.context = context;
+
+        }
+
+        public async Task<UserLike> GetUserLike(int sourceUserId, int targetUserId)
+        => await this.context.Likes
+           .FindAsync(sourceUserId, targetUserId);
+
+
+        public async Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId)
+        {
+            var users = this.context.Users.OrderBy(u => u.UserName).AsQueryable();
+
+            var likes = this.context.Likes.AsQueryable();
+
+            if (predicate == "liked")
+            {
+                likes = likes.Where(l => l.SourceUserId == userId);
+                users = likes.Select(like => like.TargetUser);
+            }
+
+            if (predicate == "likedBy")
+            {
+                likes = likes.Where(l => l.TargetUserId == userId);
+                users = likes.Select(like => like.SourceUser);
+            }
+
+            return await users.Select(user => new LikeDto
+            {
+                UserName = user.UserName,
+                KnownAs = user.KnownAs,
+                Age = user.DateOfBirth.CalculateAge(),
+                PhotUrl = user.Photos.FirstOrDefault(x => x.IsMain).Url,
+                City = user.City,
+                Id = user.Id
+            }).ToListAsync();
+        }
+
+        public async Task<AppUser> GetUserWithLikes(int userId)
+         => await this.context.Users
+           .Include(x => x.LikedUsers)
+           .FirstOrDefaultAsync(x => x.Id == userId);
+    }
+}
