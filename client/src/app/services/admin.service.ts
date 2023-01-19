a8915d4b521f49c27746d56bec8b47101f3fbc1d
IAyvazov <iayvazovl@gmail.com>
2023-01-19 22:51:17 +0200

    Adding photo management in client

--- client/src/app/services/admin.service.ts
+++ client/src/app/services/admin.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { PhotoEditorComponent } from '../members/photo-editor/photo-editor.component';
+import { Photo } from '../models/photo';
 import { User } from '../models/user';
 
 @Injectable({
@@ -17,5 +19,17 @@ export class AdminService {
 
   updateUserRolse(username: string, roles: string) {
     return this.http.post<string[]>(this.baseUrl + 'admin/edit-roles/' + username + '?roles=' + roles, {});
+  }
+
+  getPhotosForApproval(){
+    return this.http.get<Photo[]>(this.baseUrl+ 'admin/photos-to-moderate');
+  }
+
+  approvePhoto(photoId: number){
+    return this.http.post(this.baseUrl + 'admin/approve-photo/' + photoId, {});
+  }
+
+  rejectPhoto(photoId: number){
+    return this.http.post(this.baseUrl + 'admin/reject-photo/' + photoId, {});
   }
 }
